37f4c4c27d0db1ebf935b96bc366014e6a4c5654
Sergey Opekunov <sergos5@yandex.ru>
2024-12-17 14:00:58 +0300

    fix: icons svg srite

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('.logo-svg use')
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('navbar-light')
+        logo.href.baseVal = "./image/sprite.svg#logo-dark"
     } else {
         navbar.classList.remove('navbar-light')
+        logo.href.baseVal = "./image/sprite.svg#logo-light"
     }
 })
-
