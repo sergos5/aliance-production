40e9fde8149525601c5ff4fd36c6cdaebfca35e5
Sergey Opekunov <sergos5@yandex.ru>
2024-12-22 20:21:41 +0300

    add: adaptive site header

--- js/script.js
+++ js/script.js
@@ -2,12 +2,28 @@
 
 const navbar = document.querySelector('.navbar')
 const logo = document.querySelector('.logo-svg use')
+const mMenu = document.querySelector('.mobile-menu')
+const mMenuToggle = document.querySelector('.mobile-menu-toggle')
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
         navbar.classList.add('navbar-light')
         logo.href.baseVal = "./image/sprite.svg#logo-dark"
-    } else {
+    } if (this.scrollY === 0 && !mMenu.classList.contains('is-open')) {
+        navbar.classList.remove('navbar-light')
+        logo.href.baseVal = "./image/sprite.svg#logo-light"
+    }
+})
+
+mMenuToggle.addEventListener('click', (event) => {
+    event.preventDefault()
+    mMenu.classList.toggle('is-open')
+
+    if (mMenu.classList.contains('is-open')) {
+        navbar.classList.add('navbar-light')
+        logo.href.baseVal = "./image/sprite.svg#logo-dark"
+
+    } else if (!mMenu.classList.contains('is-open') && window.scrollY === 0) {
         navbar.classList.remove('navbar-light')
         logo.href.baseVal = "./image/sprite.svg#logo-light"
     }
